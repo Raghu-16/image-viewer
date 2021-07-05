92fff71e79e8b76c06f2ae904f22d33014a78871
Raghu-16 <tusharbhutada16@gmail.com>
2021-07-05 16:36:14 +0530

    Implemented profile page (part 3)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom'
 import Login from './screens/login/Login';
 import Home from './screens/home/Home';
 
+
 const App = () => (
   <Switch>
     <Route exact path='/' render={({history}, props) => <Login {...props} history={history}/>} />
     <Route exact path='/home' render={({history}, props) => <Home {...props} history={history}/>} />
+    <Route exact path='/profile' render={({history}, props) => <profile {...props} history={history}/>} />
   </Switch>
 )
 
